629fc2e9c247df6254314aa3804d1339d3b2fc74
Siamak Kosari <skosari@protonmail.com>
2020-08-03 15:57:34 -0700

    Updated delete button with an alert

--- src/App.js
+++ src/App.js
@@ -6,74 +6,97 @@ import Popup from 'reactjs-popup';
 class App extends Component {
 state = {
   isLoading: false,
-  invoices: [
+  debts: [
+
     {
-      'id': '1',
-      'vendor': 'Hancock',
-      'amount': '100',
-      'invoice': '1',
-      'date': '8/6/1980'
+      'id': '5',
+      'creditor': 'wells fargo',
+      'balance': '1450.32',
+      'originalbalance': '1920.25',
+      'rate': '4.99',
+      'payment': '87.36',
+      'goal': '0',
+      'paidoffdate': '9/9/2022',
+      'totalinterestpaid': '140.34',
+      'monthstopayoff': '6',
+      'payoffstrategy': 'snowball'
     },
     {
-      'id': '2',
-      'vendor': 'Warren',
-      'amount': '200',
-      'invoice': '2',
-      'date': '8/6/1982'
+      'id': '6',
+      'creditor': 'bank of america',
+      'balance': '1450.32',
+      'originalbalance': '1920.25',
+      'rate': '4.99',
+      'payment': '87.36',
+      'goal': '0',
+      'paidoffdate': '9/9/2022',
+      'totalinterestpaid': '140.34',
+      'monthstopayoff': '6',
+      'payoffstrategy': 'avalanche'
     },
     {
-      'id': '3',
-      'vendor': 'Hancock3',
-      'amount': '300',
-      'invoice': '3',
-      'date': '8/6/1983'
+      'id': '7',
+      'creditor': 'discover',
+      'balance': '1450.32',
+      'originalbalance': '1920.25',
+      'rate': '4.99',
+      'payment': '87.36',
+      'goal': '0',
+      'paidoffdate': '9/9/2022',
+      'totalinterestpaid': '140.34',
+      'monthstopayoff': '6',
+      'payoffstrategy': 'tableorder'
     },
     {
-      'id': '4',
-      'vendor': 'Smith',
-      'amount': '400',
-      'invoice': '4',
-      'date': '8/6/1984'
+      'id': '8',
+      'creditor': 'synchrony',
+      'balance': '1450.32',
+      'originalbalance': '1920.25',
+      'rate': '4.99',
+      'payment': '87.36',
+      'goal': '0',
+      'paidoffdate': '9/9/2022',
+      'totalinterestpaid': '140.34',
+      'monthstopayoff': '6',
+      'payoffstrategy': 'nosnowball'
     },
+    {
+      'id': '10',
+      'creditor': 'us bank',
+      'balance': '1450.32',
+      'originalbalance': '1920.25',
+      'rate': '4.99',
+      'payment': '87.36',
+      'goal': '0',
+      'paidoffdate': '9/9/2022',
+      'totalinterestpaid': '140.34',
+      'monthstopayoff': '6',
+      'payoffstrategy': 'lowestfirst'
+    }
   ],
 }
 delBtn(id) {
-  let updatedInvoices = [...this.state.invoices].filter(x => x.id !== id);
-  this.setState({ invoices: updatedInvoices })
+  let updatedDebts = [...this.state.debts].filter(x => x.id !== id);
+  this.setState({ debts: updatedDebts }); //Logic to delete the item
 }
 
-/*
-addInvoice = (e) => {
-  this.setState({
-    invoices : [...this.state.invoices, e.target.value]
-  })
-}
-paid = (e) => {
-
-}
-
-image = (e) => {
-
-}
-reconsiled = (e) => {
-
-}*/
 render() {
   const isLoading = this.state.isLoading;
   
   if(isLoading){return <div>Loading...</div>};
-  const InfoModal = () => (
+  
+  const DebtRelief = () => (
     <Popup
-      trigger={<button className="btn btn-lg btn-info"> Info <FontAwesomeIcon icon={faInfo} /></button>}
+      trigger={<button className="btn btn-lg btn-info"> Debt Relief <FontAwesomeIcon icon={faInfo} /></button>}
       modal
       closeOnDocumentClick
     >
       <span> Modal content </span>
     </Popup>
   );
-  const ReconcileModal = () => (
+  const PaymentCalculator = () => (
     <Popup
-      trigger={<button className="btn btn-lg btn-warning"> Reconcile <FontAwesomeIcon icon={faCalculator} /></button>}
+      trigger={<button className="btn btn-lg btn-warning"> Payment Calculator <FontAwesomeIcon icon={faCalculator} /></button>}
       modal
       closeOnDocumentClick
     >
@@ -89,27 +112,24 @@ render() {
       <span> Modal Content </span>
     </Popup>
   );
-  const CreditorAccount = () => (
-    <Popup
-      trigger={<button className="btn btn-lg btn-success"> Account <FontAwesomeIcon icon={faCheckSquare} /></button>}
-      modal
-      closeOnDocumentClick
-    >
-      <span style={{color:'black'}}> Modal Content is not showing up because we need to change the font color</span>
-    </Popup>
-  );
-  const allInvoices = this.state.invoices;
-  let invoiceControls = allInvoices.map(x => 
+
+  const allDebts = this.state.debts;
+  let invoiceControls = allDebts.map(x => 
     <tr key={x.id}>
-      <td>{x.vendor}</td>
-      <td>{x.amount}</td>
-      <td>{x.invoice}</td>
-      <td>{x.date}</td>
+      <td>{x.creditor}</td>
+      <td>{x.balance}</td>
+      <td>{x.payment} </td>
+      <td>{x.rate}</td>
+      <td>{x.monthstopayoff}</td>
+      <td>{x.originalbalance}</td>
+      <td>{x.paidoffdate}</td>
+      <td>{x.totalinterestpaid}</td>
       <td><ModifyModal /></td>
-      <td><InfoModal /></td>
-      <td><ReconcileModal /></td>
-      <td><Button className="btn btn-lg btn-danger" onClick={()=>this.delBtn(x.id)}>Delete <FontAwesomeIcon icon={faTrashAlt}/></Button></td>
-      <td><CreditorAccount /></td>    
+      <td><DebtRelief /></td>
+      <td><PaymentCalculator /></td>
+      <td>
+        <Button className="btn btn-lg btn-danger" onClick={()=>{ if (window.confirm('Are you sure you wish to delete this item?')) this.delBtn(x.id) } }>Delete <FontAwesomeIcon icon={faTrashAlt} /></Button>
+      </td>
     </tr>
   );
 
@@ -117,11 +137,11 @@ render() {
 
     <div className='container border border-secondary rounded secondary' >
       
-      <h1>This is a bills app being built using React</h1>
+      <h1>Credit Debt Reduction app built using React</h1>
       
       <div className='row'>
         <div className='col-12'>
-          <h4>Pending bills - KosariCo</h4>
+          <h4>Credit Debt Reduction Assistant</h4>
         </div>
       </div>
 
@@ -130,17 +150,21 @@ render() {
           <Table dark responsive bordered hover>
             <thead>
               <tr>
-                <th>Vendor</th>
-                <th>Amount</th>
-                <th>Invoice</th>
-                <th>Date</th>
-                <th colSpan='4'>Actions</th>
+                <th>Creditor</th>
+                <th>Balance</th>
+                <th>Monthly Payment</th>
+                <th>Interest Rate</th>
+                <th>Months to Pay Off</th>
+                <th>Original Balance</th>
+                <th>Pay Off Date</th>
+                <th>Total Interest Paid</th>
+                <th colSpan='5'>Actions</th>
                 <th>image</th>
               </tr>
             </thead>
             <tbody>
               
-              {this.state.invoices.length === 0 ? <tr><td colSpan='9'>All Caught Up</td></tr> : invoiceControls}
+              {this.state.debts.length === 0 ? <tr><td colSpan='9'>All Caught Up</td></tr> : invoiceControls}
               
             </tbody>
           </Table>
